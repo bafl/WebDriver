5562f27b83dd0c8783ced17b2dff3a62d8fb0394
Bartlomiej <bartlomiej.florczak@qcadoo.com>
2016-04-03 13:49:25 +0200

    Static import of expected condition

--- src/main/java/com/qcadoo/webdriver/pageobject/basic/products/ProductsListPO.java
+++ src/main/java/com/qcadoo/webdriver/pageobject/basic/products/ProductsListPO.java
@@ -1,10 +1,12 @@
 package com.qcadoo.webdriver.pageobject.basic.products;
 
 import com.qcadoo.webdriver.pageobject.AbstractPageObject;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 /**
  * WebDriver
@@ -15,13 +17,15 @@ public class ProductsListPO extends AbstractPageObject {
     @FindBy(xpath = "//label[contains(.,'Dodajnowy')]")
     private WebElement addNewProduct;
 
+    By addProduct = new By.ByXPath("//label[contains(.,'Dodajnowy')]");
+
 
     public ProductsListPO(WebDriver driver) {
         super(driver);
     }
 
     public ProductsDetailsPO addNewProduct() {
-        wait.until(ExpectedConditions.elementToBeClickable(addNewProduct)).click();
+        wait.until(elementToBeClickable(addNewProduct)).click();
         return new ProductsDetailsPO(driver);
     }
 }
