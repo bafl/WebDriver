a1a6959ad3136bea1b0f480c94610bed694b0f7f
Bartlomiej <bartlomiej.florczak@qcadoo.com>
2016-04-07 23:10:08 +0200

    typeDecription, selectUnit added

--- src/main/java/com/qcadoo/webdriver/pageobject/basic/products/ProductsDetailsPO.java
+++ src/main/java/com/qcadoo/webdriver/pageobject/basic/products/ProductsDetailsPO.java
@@ -1,8 +1,10 @@
 package com.qcadoo.webdriver.pageobject.basic.products;
 
 import com.qcadoo.webdriver.pageobject.AbstractPageObject;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
@@ -12,6 +14,7 @@ import org.openqa.selenium.support.ui.Select;
  */
 public class ProductsDetailsPO extends AbstractPageObject {
 
+    @CacheLookup
     @FindBy(id = "window.mainTab.product.gridLayout.number_input")
     private WebElement productNumberInput;
 
@@ -21,12 +24,48 @@ public class ProductsDetailsPO extends AbstractPageObject {
     @FindBy(xpath = "//span[contains(.,'Zapisz')]")
     private WebElement saveProductButton;
 
-    @FindBy (id = "window.mainTab.product.gridLayout.globalTypeOfMaterial_input")
+    @FindBy(id = "window.mainTab.product.gridLayout.globalTypeOfMaterial_input")
     private WebElement globalTypeOfMaterialSelect;
+
+    @FindBy(id = "window.mainTab.product.gridLayout.description_input")
+    private WebElement descriptionInput;
+
+    @FindBy(id = "window.mainTab.product.gridLayout.unit_input")
+    private WebElement productUnitSelect;
 
 
     public ProductsDetailsPO(WebDriver driver) {
         super(driver);
+    }
+
+    public ProductsDetailsPO typeProductDescription(String productDescription) {
+        clearAndType(descriptionInput, productDescription);
+        return this;
+    }
+
+    public ProductsDetailsPO typeProductNameAndNumber(String number, String name) {
+        waitForLoad();
+        clearAndType(productNumberInput, number);
+        clearAndType(productNameInput, name);
+        return this;
+    }
+
+    public ProductsDetailsPO selectGlobalTypeOfMaterial(GlobalTypeOfMaterial materialType) {
+        Select globalTypeOfMaterial = new Select(globalTypeOfMaterialSelect);
+        globalTypeOfMaterial.selectByValue(materialType.getValue());
+        return this;
+    }
+
+    public ProductsDetailsPO selectProductUnit(String unit) {
+        Select productUnitSelect = new Select(this.productUnitSelect);
+        if (!productUnitSelect.getFirstSelectedOption().getText().equals(unit)) {
+            try {
+                productUnitSelect.selectByVisibleText(unit);
+            } catch (NoSuchElementException e) {
+                e.printStackTrace();
+            }
+        }
+        return this;
     }
 
     public enum GlobalTypeOfMaterial {
@@ -42,18 +81,5 @@ public class ProductsDetailsPO extends AbstractPageObject {
             return value;
         }
 
-    }
-
-    public ProductsDetailsPO typeProductNameAndNumber(String number, String name) {
-        waitForLoad();
-        clearAndType(productNumberInput, number);
-        clearAndType(productNameInput, name);
-        return this;
-    }
-
-    public ProductsDetailsPO selectGlobalTypeOfMaterial(GlobalTypeOfMaterial materialType) {
-        Select globalTypeOfMaterial = new Select(globalTypeOfMaterialSelect);
-        globalTypeOfMaterial.selectByValue(materialType.getValue());
-        return this;
     }
 }
