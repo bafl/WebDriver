9e56febd2f3dca2ca5060116788fb0be106cec0d
Bartlomiej <bartlomiej.florczak@qcadoo.com>
2016-04-11 22:40:08 +0200

    FF extensions attached

--- src/main/java/com/qcadoo/webdriver/drivermanager/WebDriverThread.java
+++ src/main/java/com/qcadoo/webdriver/drivermanager/WebDriverThread.java
@@ -3,11 +3,16 @@ package com.qcadoo.webdriver.drivermanager;
 import org.junit.AfterClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.io.File;
 
 public class WebDriverThread {
 
     private static WebDriver webDriver;
+
+    private static final File firebug_path = new File("./src/main/resources/firebug-2.0.16-fx.xpi");
+    private static final File firepath_path = new File("./src/main/resources/firepath-0.9.7.1-fx.xpi");
 
     private static final String operatingSystem = System.getProperty("os.name").toUpperCase();
     private static final String systemArchitecture = System.getProperty("os.arch");
@@ -19,11 +24,26 @@ public class WebDriverThread {
             System.out.println("Current Architecture: " + systemArchitecture);
             System.out.println("Current Browser Selection: Firefox");
             System.out.println(" ");
-            webDriver = new FirefoxDriver(DesiredCapabilities.firefox());
+            FirefoxProfile firefoxProfile = new FirefoxProfile();
+            firefoxProfile.addExtension(firebug_path);
+            firefoxProfile.addExtension(firepath_path);
+
+            String ext = "extensions.firebug.";
+            String ext1 = "extensions.firepath.";
+
+            firefoxProfile.setPreference(ext + "currentVersion", "2.0.16");
+            firefoxProfile.setPreference(ext1 + "currentVersion", "0.9.7.1");
+            firefoxProfile.setPreference(ext + "allPagesActivation", "on");
+            firefoxProfile.setPreference(ext + "defaultPanelName", "net");
+            firefoxProfile.setPreference(ext + "net.enableSites", true);
+
+            webDriver = new FirefoxDriver(firefoxProfile);
+            webDriver.manage().window().maximize();
         }
 
         return webDriver;
     }
+
     @AfterClass
     public static void quitDriver() {
         if (null != webDriver) {
